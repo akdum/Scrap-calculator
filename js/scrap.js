087ced7ef99b3f1072778cb3d42c9561b937798d
Ivan Steblenko <IvanSteblenko@hotmail.com>
2017-01-03 14:30:43 +0400

    add images

--- js/scrap.js
+++ js/scrap.js
@@ -7,4 +7,27 @@
         }, 1000);
         e.preventDefault();
     })
+
+    window.sr = ScrollReveal();
+
+    sr.reveal(".sr-icons", {
+        duration: 1600,
+        scale: .1,
+        distance: "0px"
+    }, 200);
+
+    a(".popup-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Загружается изображение #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+        enabled: !0,
+        navigateByImgClick: !0,
+        preload: [0, 1]
+    },
+    image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+})
 }(jQuery);
